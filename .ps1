8c3f89655c0626be985ac6f253b1bc28b92253be
cjijcb <cjijcb@gmail.com>
2026-06-21 16:44:44 +0800

    Refactor PS1 prompt colors

--- .ps1
+++ .ps1
@@ -6,9 +6,8 @@
 
 # Color - raw ANSI
 nc=$'\033[0m'
-green=$'\033[1;32m'
-yellow=$'\033[0;33m'
-blue=$'\033[1;34m'
+red=$'\033[38;5;124m'
+grey=$'\033[38;5;242m'
 
 # Color - PS1 usage
 ps() { printf '\[%s\]' "$1"; }
@@ -18,21 +17,23 @@ git_branch() {
   branch=$(git branch --show-current 2>/dev/null) || return
   [[ -n "$branch" ]] || return
   printf '%s(%s%s%s)%s' \
-    "$yellow" \
+    "$red" \
     "$nc" \
     "$branch" \
-    "$yellow" \
+    "$red" \
     "$nc"
 }
 
 PS1=""
-PS1+="$(ps "$green")┌──(\u"  # ┌──(username
-PS1+="$(ps "$nc")@"          # @
-PS1+="$(ps "$blue")\h)-["    # hostname)-[
-PS1+="$(ps "$nc")\w"         # ~/workdir
-PS1+="$(ps "$blue")]"        # ]
+PS1+="$(ps "$red")┌──("   # ┌──(
+PS1+="$(ps "$red")\u"     # username
+PS1+="$(ps "$grey")@"     # @
+PS1+="$(ps "$red")\h"     # hostname
+PS1+="$(ps "$red"))-["    # )-[
+PS1+="$(ps "$nc")\w"      # ~/workdir
+PS1+="$(ps "$red")]"      # ]
 PS1+="$(ps "$nc")"
-PS1+='$(git_branch)'         # (branch) or nothing
+PS1+='$(git_branch)'      # (branch) or nothing
 PS1+=$'\n'
-PS1+="$(ps "$green")└─"      # └─
-PS1+="$(ps "$nc")\$ "        # $
+PS1+="$(ps "$red")└─$"    # └─$
+PS1+="$(ps "$nc") "
